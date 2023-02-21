97e5fe47bcba66b2ff5266f0fb4143f1924198fe
lindajani <lindajani02@gmail.com>
2023-02-20 18:18:53 -0800

    Alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphbt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'p' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
