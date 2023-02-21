acc300a58a880df7de13e5030293883baf5d9b51
lindajani <lindajani02@gmail.com>
2023-02-20 17:31:49 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,14 +2,14 @@
 
 /**
  * main - A program that prints the alphabet
- * Return:0 (Success)
+ * Return:0 (Sucess)
  */
 
 int main(void)
-{       
-        char i;
-	for (i = 'a'; i <= 'z'; i++)
-		putchar(i);
-	putchar('\n');
-	return (0);
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+return (0);
 }
