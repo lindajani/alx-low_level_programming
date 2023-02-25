3b762d7b96340859ce6a78971d646014f698ba36
lindajani <lindajani02@gmail.com>
2023-02-24 19:50:45 -0800

    print diagonals

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,19 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * __isdigit - checks if parameter is a number between 0 to 9.
- * @c: input number
- *
- * Return: 1 if c is a digit, 0 otherwise
+ * _isdigit - checks if parameter is a number between 0 to 9.
+ * @c: input number.
+ * Return: 1 if is a number (0 to 9), 0 in other case.
  */
-
-int _isdigit(int c)
+int _isdigit(int c);
 {
-	if (c >= '0' && c <= '9')
-		printf("%c: %d\n", c, _isdigit(c)) c = 'a';
-		return (1);
-	else
-		return (0);
-
+if (c >= 48 && c <= 57)
+{
+return (1);
 }
+else
+{
+return (0);
+}
+}
+
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_diagonal - prints diagonal line n times.
  * @n: times diagonal line is printed.
@@ -13,12 +13,11 @@ for (i = 0; i < n; i++)
 {
 for (j = 0; j < i; j++)
 {
+putchar(' ');
+}
 putchar(92);
 if (i < (n - 1))
 putchar('\n');
 }
 putchar('\n');
 }
-}
-
-
