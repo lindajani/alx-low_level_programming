e1a809009f4a078e1526909a855872716492364b
lindajani <lindajani02@gmail.com>
2023-03-02 04:12:32 -0800

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
  *
  * Return: n
  */
-
 char *string_toupper(char *n)
 {
 	int i;
@@ -14,9 +12,9 @@ char *string_toupper(char *n)
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'Z')
+		if (n[i] >= 'a' && n[i] <= 'z')
 			n[i] = n[i] - 32;
 		i++;
 	}
-return (0);
+	return (n);
 }
